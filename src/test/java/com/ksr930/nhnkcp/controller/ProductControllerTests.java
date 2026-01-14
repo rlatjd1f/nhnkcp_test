49807f9e9794960927383a21202b4fa66c871fe6
rlatj <dl290382@gmail.com>
2026-01-14 22:48:11 +0900

    test: MockBean 대체 MockitoBean 적용

--- src/test/java/com/ksr930/nhnkcp/controller/ProductControllerTests.java
+++ src/test/java/com/ksr930/nhnkcp/controller/ProductControllerTests.java
@@ -20,10 +20,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ProductController.class)
@@ -34,7 +34,7 @@ class ProductControllerTests {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@MockBean
+	@MockitoBean
 	private ProductService productService;
 
 	@Test
